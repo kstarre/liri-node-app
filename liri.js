920f05cd1db697d48e73ee3f2bdd98a466af9e45
Katie Starrett <starrett.k.e@gmail.com>
2017-07-06 00:57:05 -0400

    add all preliminary files

---
+++ liri.js
@@ -0,0 +1,35 @@
+const keysFile = require('./keys');
+const twitterKeys = keysFile.twitterKeys;
+
+let command = process.argv[2];
+
+function recentTweets() {
+
+};
+
+function songInfo() {
+
+};
+
+function movieInfo() {
+
+};
+
+function randomSong() {
+
+};
+
+switch( command ) {
+	case "my-tweets":
+
+	break;
+	case "spotify-this-song":
+
+	break;
+	case "movie-this":
+
+	break;
+	case "do-what-it-says":
+
+	break;
+}
